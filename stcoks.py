05fdce6b99200c324a0161682dec71d2dd48ecbd
simge <103750610+simge1999@users.noreply.github.com>
2022-07-10 12:42:57 +0300

    Add files via upload

---
+++ stcoks.py
@@ -0,0 +1,9 @@
+apple_stocks = [138,823, 124, 213.333,105.87]
+apple_stocks[0] = 310
+print("latest value:")
+print(apple_stocks[0])
+apple_stocks[1] = 310
+print("Highest value:")
+print(apple_stocks[1])
+print("Lowest value:")
+print(apple_stocks[4])
